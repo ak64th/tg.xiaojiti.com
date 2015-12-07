dc73de4c7aec276ca4825fa32ad7e5f2be8c5e23
郁骏 <ak64th@gmail.com>
2015-12-07 14:36:20 +0800

    import umysqldb when needed

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask
-import umysqldb
-umysqldb.install_as_MySQLdb()
-from flask_peewee.db import Database
 from config import Configuration
 
 app = Flask(__name__)
 app.config.from_object(Configuration)
+
+if app.config['DATABASE']['engine'] == 'peewee.MySQLDatabase':
+    import umysqldb
+    umysqldb.install_as_MySQLdb()
+
+from flask_peewee.db import Database
 
 db = Database(app)
