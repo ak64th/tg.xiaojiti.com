2fa451a1dbbedc6abcf118838b88ecba9ef75c25
郁骏 <ak64th@gmail.com>
2015-12-07 15:03:19 +0800

    remove umysqldb for runtime errors

--- app.py
+++ app.py
@@ -1,13 +1,8 @@
 from flask import Flask
+from flask_peewee.db import Database
 from config import Configuration
 
 app = Flask(__name__)
 app.config.from_object(Configuration)
-
-if app.config['DATABASE']['engine'] == 'peewee.MySQLDatabase':
-    import umysqldb
-    umysqldb.install_as_MySQLdb()
-
-from flask_peewee.db import Database
 
 db = Database(app)
